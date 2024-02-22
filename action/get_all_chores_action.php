04dc79f54fa5f34a76f0e013d79b155143d9861c
P-Asare <pinopalal11@gmail.com>
2024-02-22 00:15:30 +0000

    create all chores function

---
+++ action/get_all_chores_action.php
@@ -0,0 +1,21 @@
+<?php
+
+    include("../settings/connection.php");
+
+    function all_chores(){
+        global $conn;
+        
+        $sql = "SELECT * FROM chores";
+
+        $result = mysqli_query($conn, $sql);
+
+        if (!$result){
+            die("Unsuccessful Query: ". mysqli_error($conn));
+        }
+        else if (mysqli_num_rows($result) > 0){
+            $chores = mysqli_fetch_all($result, MYSQLI_ASSOC);
+            return $chores;
+        }
+    }
+
+?>
