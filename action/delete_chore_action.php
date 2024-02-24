e66d7030d80d7b71dff57a75cbe5f619f0fe5dad
P-Asare <pinopalal11@gmail.com>
2024-02-24 15:59:57 +0000

    enable delete chore from database

--- action/delete_chore_action.php
+++ action/delete_chore_action.php
@@ -1,3 +1,23 @@
 <?php
-    echo "working";
+    include("../settings/connection.php");
+
+    if (isset($_GET['id'])){
+
+        $chore_id = $_GET['id'];
+        $sql = "DELETE FROM chores WHERE cid = ?";
+
+        $stmt = mysqli_prepare($conn, $sql);
+        mysqli_stmt_bind_param($stmt, "i", $chore_id);
+        mysqli_stmt_execute($stmt);
+        
+        $result = mysqli_stmt_get_result($stmt);
+
+        if ($result){
+            header("Location: ../admin/chore_control_view.php");
+        }
+        else{
+
+            header("Location: ../admin/chore_control_view.php?msg=dcerror");
+        }
+    }
 ?>
