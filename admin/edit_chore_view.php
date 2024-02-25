0dc3dbb4feb252a4d8e60dc7f96cb64725301a4b
P-Asare <pinopalal11@gmail.com>
2024-02-25 09:36:14 +0000

    implemented reroute to edit_chore_view

--- admin/edit_chore_view.php
+++ admin/edit_chore_view.php
@@ -1,3 +1,27 @@
 <?php
-    echo "working";
+    include("../settings/core.php");
+    // check_login();
+    include("../action/get_a_chore_action.php");
+
+    if (isset($_GET['id'])){
+        $c_id = $_GET['id'];
+        $id_row = get_chore($c_id);
+    }
+    else {
+        header("Location: ../admin/chore_control_view.php");
+    }
+    
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Edit chore</title>
+</head>
+<body>
+
+
+    
+</body>
+</html>
