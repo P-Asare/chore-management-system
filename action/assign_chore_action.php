51ba3eb5cfd287c7940e46b5ef5b29c28f45e1a8
P-Asare <pinopalal11@gmail.com>
2024-03-12 18:06:03 +0000

    write assignment details into database without session id working

--- action/assign_chore_action.php
+++ action/assign_chore_action.php
@@ -1,5 +1,22 @@
 <?php
     include("../settings/connection.php");
 
-    echo "working";
+    if(isset($_POST['submit'])){
+
+        $chore = $_POST['chore'];
+        $status = 1;
+        $date_assigned = date('m-d-Y');
+        $due_date = $_POST['due-date'];
+        $assigned_by = $_SESSION['user-id'];
+        $assigned_to = $_POST['assignee'];
+
+        // Insert into assignment database
+        $sql = "INSERT INTO assignment (cid, sid, date_assign, date_due, who_assigned) VALUES (?,?,?,?,?)";
+        $stmt = mysqli_prepare($conn, $sql);
+        mysqli_stmt_bind_param($stmt, "iissi", $chore, $status, $date_assigned, $due_date, $assigned_by);
+        mysqli_stmt_execute($stmt);
+
+
+
+    }
 ?>
