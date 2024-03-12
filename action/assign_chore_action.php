e18ec3d7db16e0ed7f71b31a36b88c81a2714551
P-Asare <pinopalal11@gmail.com>
2024-03-12 18:29:18 +0000

    insert assignee in assigned people relation

--- action/assign_chore_action.php
+++ action/assign_chore_action.php
@@ -5,7 +5,7 @@
 
         $chore = $_POST['chore'];
         $status = 1;
-        $date_assigned = date('m-d-Y');
+        $date_assigned = date('Y-m-d');
         $due_date = $_POST['due-date'];
         // $assigned_by = $_SESSION['user-id'];
         $assigned_to = $_POST['assignee'];
@@ -20,9 +20,11 @@
 
         $assignmentId = mysqli_insert_id($conn); // Get id of most recent insertion
 
-
-
-
+        // Update the relation of individuals with assigned tasks
+        $sql2 = "INSERT INTO assigned_people (pid, assignmentid) VALUES (?, ?)";
+        $stmt2 = mysqli_prepare($conn, $sql2);
+        mysqli_stmt_bind_param($stmt2, "ii", $assigned_to, $assignmentId);
+        mysqli_stmt_execute($stmt2);
 
     }
 ?>
