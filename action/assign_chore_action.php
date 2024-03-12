044263b385ae24c971d36267bb016aa8e77256b3
P-Asare <pinopalal11@gmail.com>
2024-03-12 18:25:09 +0000

    get assignment id from current insertion

--- action/assign_chore_action.php
+++ action/assign_chore_action.php
@@ -7,14 +7,20 @@
         $status = 1;
         $date_assigned = date('m-d-Y');
         $due_date = $_POST['due-date'];
-        $assigned_by = $_SESSION['user-id'];
+        // $assigned_by = $_SESSION['user-id'];
         $assigned_to = $_POST['assignee'];
+        
+        $ass = 2; // Test value due to unavailable session
 
         // Insert into assignment database
         $sql = "INSERT INTO assignment (cid, sid, date_assign, date_due, who_assigned) VALUES (?,?,?,?,?)";
         $stmt = mysqli_prepare($conn, $sql);
-        mysqli_stmt_bind_param($stmt, "iissi", $chore, $status, $date_assigned, $due_date, $assigned_by);
+        mysqli_stmt_bind_param($stmt, "iissi", $chore, $status, $date_assigned, $due_date, $ass);
         mysqli_stmt_execute($stmt);
+
+        $assignmentId = mysqli_insert_id($conn); // Get id of most recent insertion
+
+
 
 
 
